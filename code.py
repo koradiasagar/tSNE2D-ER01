03a914c7d664ee563e8b2272a64e158205c15b24
Sagar Koradia <31327738+SagarKoradia@users.noreply.github.com>
2017-10-16 21:45:13 +1100

    Create code.py

---
+++ code.py
@@ -0,0 +1,18 @@
+from numpy import genfromtxt
+from sklearn.manifold import TSNE
+import matplotlib.pyplot as plt
+from sklearn.cluster import KMeans
+
+fn = r'C:\Users\DELL I5558\Desktop\Python\NSW-ER01.csv'
+my_data = genfromtxt(fn, delimiter=',')
+model = KMeans(n_clusters=4)
+model.fit(my_data)
+labels = model.predict(my_data)
+print(labels)
+model = TSNE(n_components=3, perplexity=40, learning_rate=100, n_iter=1000, random_state=0)
+transformed = model.fit_transform(my_data)
+xs = transformed[:, 0]
+ys = transformed[:, 1]
+
+plt.scatter(xs, ys, c=labels)
+plt.show()
